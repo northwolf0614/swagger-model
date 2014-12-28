f9dc4ea50db27b9da1bf5b0621efb4680f20a3bd
Ran Ding <randing89@gmail.com>
2014-12-28 14:13:56 +1100

    Fix problem in converting date

--- template/helper.js
+++ template/helper.js
@@ -6,9 +6,9 @@ module.exports = {
             return date;
         }
 
-        var yyyy = this.getFullYear().toString();
-        var mm = (this.getMonth() + 1).toString();
-        var dd = this.getDate().toString();
+        var yyyy = date.getFullYear().toString();
+        var mm = (date.getMonth() + 1).toString();
+        var dd = date.getDate().toString();
 
         return yyyy + '-' + (mm[1] ? mm : '0' + mm[0]) + '-' + (dd[1] ? dd : '0' + dd[0]);
     },
