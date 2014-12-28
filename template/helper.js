b05f2c6e2753f76f187d702e6667739bdd7dc8ec
Ran Ding <randing89@gmail.com>
2014-12-28 14:11:48 +1100

    Change date format from yyyymmdd to yyyy-mm-dd

--- template/helper.js
+++ template/helper.js
@@ -1,21 +1,25 @@
 module.exports = {
     date2Ymd: function (date) {
+        if (!date) return '';
+
         if (typeof date === 'string') {
             return date;
         }
 
-        var yyyy = date.getFullYear();
-        var mm = date.getMonth() + 1;
-        var dd = date.getDate();
+        var yyyy = this.getFullYear().toString();
+        var mm = (this.getMonth() + 1).toString();
+        var dd = this.getDate().toString();
 
-        return String(10000 * yyyy + 100 * mm + dd);
+        return yyyy + '-' + (mm[1] ? mm : '0' + mm[0]) + '-' + (dd[1] ? dd : '0' + dd[0]);
     },
 
     ymd2Date: function (ymd) {
+        if (!ymd) return undefined;
+
         var r = ymd.split('-');
 
         if (r.length !== 3) {
-            return Number.Nan;
+            return undefined;
         }
 
         return new Date(r[0], r[1] - 1, r[2]);
